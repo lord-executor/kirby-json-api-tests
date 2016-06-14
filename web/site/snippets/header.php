1b03dee25bb30f8e7b89ff2627b30063e20b1b85
Lukas Angerer <lukas.angerer@getunik.com>
2016-06-14 15:06:49 +0200

    Added some basic content, templates and the JSON API plugin

---
+++ web/site/snippets/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+	<title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+	<meta name="description" content="<?php echo $site->description()->html() ?>">
+	<meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
+
+	<?php echo css('assets/css/styles.css') ?>
+
+</head>
+<body>
+
+	<header class="header cf" role="banner">
+		<?php snippet('menu') ?>
+	</header>
