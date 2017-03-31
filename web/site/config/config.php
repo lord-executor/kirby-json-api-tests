ba7d67983292f2518d0186a84d72ca5cd2b3cec7
Lukas Angerer <lukas.angerer@gmail.com>
2017-03-31 22:40:28 +0200

    Added language configuration that was missing from the last commit

--- web/site/config/config.php
+++ web/site/config/config.php
@@ -29,6 +29,22 @@ of the system, please check out http://getkirby.com/docs/advanced/options
 
 */
 
+c::set('languages', array(
+	array(
+		'code'    => 'en',
+		'name'    => 'English',
+		'default' => true,
+		'locale'  => 'en_US',
+		'url'     => '/',
+	),
+	array(
+		'code'    => 'de',
+		'name'    => 'Deutsch',
+		'locale'  => 'de_DE',
+		'url'     => '/de',
+	),
+));
+
 /*
 JSON API Configuration
 */
@@ -36,3 +52,8 @@ c::set('jsonapi.built-in.enabled', true);
 // this is for demonstration purposes ONLY - in any kind of "real world" application
 // this should be set to _some_ form of authentication as described in the documentation
 c::set('jsonapi.built-in.auth', false);
+// set this to _false_ if you want to disable automatic detection & processing of structured fields
+c::set('jsonapi.auto-structured', true);
+
+
+c::set('debug', true);
