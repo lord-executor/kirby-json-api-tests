23b2479f75016b5e253cf0a4dc69634db2232b63
Lukas Angerer <lukas.angerer@gmail.com>
2017-04-02 12:25:17 +0200

    Updated base template header

--- web/site/snippets/header.php
+++ web/site/snippets/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php echo $site->language()->code(); ?>">
 <head>
 
 	<meta charset="utf-8" />
@@ -10,6 +10,30 @@
 	<meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
 	<?php echo css('assets/css/styles.css') ?>
+
+	<script type="text/javascript">
+		/**
+		 * Kirby JavaScript settings object. It contains settings and functions that JavaScript code
+		 * potentially needs to know about when dealing with Kirby
+		 */
+		window.Kirby = {
+			lang: '<?php echo $site->language()->code(); ?>',
+			apiPrefix: '<?php echo c::get('jsonapi.prefix', 'api'); ?>',
+			baseUrl: '<?php echo $site->url(); ?>',
+			langUrl: '<?php echo $site->language()->url(); ?>',
+			url: function (path) {
+				return this.baseUrl + (path && path[0] === '/' ? path : '/' + path);
+			},
+			pageUrl: function (path) {
+				return this.langUrl + (path && path[0] === '/' ? path : '/' + path);
+			},
+			apiUrl: function (path) {
+				var url = this.baseUrl + '/' + this.apiPrefix + (path && path[0] === '/' ? path : '/' + path);
+				url += (url.indexOf('?') < 0 ? '?' : '&') + 'lang=' + this.lang;
+				return url;
+			},
+		};
+	</script>
 
 </head>
 <body>
